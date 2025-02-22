40507c0279d172f7a38e52d542cd7b2cdecceba8
rodrigong1 <rodrigong@ext.inditex.com>
2025-02-22 20:49:08 +0100

    url terminate

--- chat/urls.py
+++ chat/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import index, response
+from .views import index, response, terminate
 
 urlpatterns = [
     path('', index, name='index'),
     path('response', response, name='response'),
+    path('terminate', terminate, name='terminate'),
 ]
