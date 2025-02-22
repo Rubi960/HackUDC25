f32fb990b0d98fcd96f8d08652c98868b7cc09a6
rodrigong1 <rodrigong@ext.inditex.com>
2025-02-23 00:48:20 +0100

    correcciones

--- Login/forms.py
+++ Login/forms.py
@@ -6,20 +6,20 @@ from django import forms
 class RegisterForm(UserCreationForm):
     username = forms.CharField(required=True, 
                                label="Usuario",
-                               widget=forms.TextInput(attrs={'class': 'input-centrado form-control', 'placeholder': 'Usuario'})
+                               widget=forms.TextInput(attrs={'class': 'input-centrado form-control', 'placeholder': 'Username'})
     )
     email = forms.EmailField(required=True, label="Email",
                                widget=forms.TextInput(attrs={'class': 'input-centrado form-control', 'placeholder': 'Email'})
     )
     first_name = forms.CharField(required=True, label="Nombre",
-                               widget=forms.TextInput(attrs={'class': 'input-centrado form-control', 'placeholder': 'Nombre'})
+                               widget=forms.TextInput(attrs={'class': 'input-centrado form-control', 'placeholder': 'First name'})
     )
     password1 = forms.CharField(    
         widget=forms.PasswordInput(attrs={'class': 'input-centrado form-control', 'placeholder': 'Password'}),
         label="Contraseña",
-        help_text='<ul><li>La contraseña debe tener como mínimo 8 caracteres</li><li>La contraseña no puede ser solo numérica</li><li>La contraseña no puede ser muy común</li></ul>',
+        help_text='<ul><li>Passwords must be longer than 8 characters</li><li>Passwords can\'t have only numbers</li><li>Passwords must not be common</li></ul>',
     )
-    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'input-centrado form-control', 'placeholder': 'Re-enter Password'}), label="Contraseña")
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'input-centrado form-control', 'placeholder': 'Re-enter Password'}), label="Password")
 
     class Meta:
         model = User
