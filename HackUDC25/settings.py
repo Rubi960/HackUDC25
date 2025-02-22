920cb3facd8f2c2f651ada7ecf1d1a4ed969a72c
Rubi960 <ruben.diz.m@gmail.com>
2025-02-22 13:38:01 +0100

    Añadido el static

--- HackUDC25/settings.py
+++ HackUDC25/settings.py
@@ -118,7 +118,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+STATIC_ROOT = ''
+
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = ('static',)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
