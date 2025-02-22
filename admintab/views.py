5ba5cba2abbb6bb0d856668bbba6f5a11d6fd023
Rubi960 <ruben.diz.m@gmail.com>
2025-02-23 00:32:53 +0100

    Ligeros cambios visuales

--- admintab/views.py
+++ admintab/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import user_passes_test
+import matplotlib.pyplot as plt
+
+from io import BytesIO
+import base64
+
+def graph_to_file(plot):
+    buffer = BytesIO()
+    plot.savefig(buffer, format='png',bbox_inches='tight')
+    buffer.seek(0)
+    image_png=buffer.getvalue()
+    graph=base64.b64encode(image_png).decode('utf-8')
+    buffer.close()
+    plt.clf()
+    return graph
 
 def es_admin(user):
     """
@@ -22,3 +36,13 @@ def admin_view(request):
     This view is only accessible if the user is authenticated and has superuser status.
     """
     return render(request, "admintab/admin.html")
+
+def your_view(request):
+    images = [
+        'img/image1.svg',
+        'img/image2.svg',
+        'img/image3.svg',
+    ]
+
+    # Pasa el contexto con las imágenes al template
+    return render(request, 'your_app/your_template.html', {'images': images})
