ca09eee6a2673303f570722b59a6c50a32365070
Rubi960 <ruben.diz.m@gmail.com>
2025-02-22 12:57:08 +0100

    Campo nombre añadido al register

--- Login/forms.py
+++ Login/forms.py
@@ -5,10 +5,11 @@ from django import forms
 class RegisterForm(UserCreationForm):
     username=forms.CharField(required=True, label='Usuario')
     email= forms.EmailField(required=True, label='Email')
+    first_name= forms.CharField(required=True, label='Nombre')
     password1=forms.CharField(widget=forms.PasswordInput(),label='Contraseña', 
                               help_text='<div id="errores"><p>La contraseña debe tener como mínimo 8 caracteres</p><p>La contraseña no puede ser solo numérica</p><p>La contraseña no puede ser muy común</p></div>')
     password2=forms.CharField(widget=forms.PasswordInput(),label='Contraseña')
     
     class Meta():
         model = User
-        fields = ["username", "password1", "password2"]
+        fields = ["username", "email", "first_name", "password1", "password2"]
